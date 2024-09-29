286899d9e6cab1c235c47fda3646b8fadaa14558
ksu_kotovaa <ksu_kotovaa@mail.ru>
2024-09-29 16:20:29 +0300

    add library

---
+++ WEB_253503_KOTOVA.Domain/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_253503_KOTOVA.Domain.Entities
+{
+    internal class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NormalizedName { get; set; }
+        public ICollection<Dish> Dishes { get; set; }
+
+        public Category()
+        {
+            Dishes = new List<Dish>();
+        }
+    }
+}
---
+++ WEB_253503_KOTOVA.Domain/Entities/Dish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_253503_KOTOVA.Domain.Entities
+{
+    internal class Dish
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } 
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
+        public string? MimeType { get; set; }
+    }
+}
