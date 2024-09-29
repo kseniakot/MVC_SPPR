b18f42b1c2404b26a20091ed3c37919dc899e8d9
ksu_kotovaa <ksu_kotovaa@mail.ru>
2024-09-29 16:45:29 +0300

    add ui project

---
+++ WEB_253503_KOTOVA.Domain/Models/ProductListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_253503_KOTOVA.Domain.Models
+{
+    internal class ProductListModel
+    {
+    }
+}
---
+++ WEB_253503_KOTOVA.UI/Extensions/HostingExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WEB_253503_KOTOVA.UI.Extensions
+{
+    public static class HostingExtensions
+    {
+        public static void RegisterCustomServices(
+        this WebApplicationBuilder builder)
+        {
+        }
+    }
+}
