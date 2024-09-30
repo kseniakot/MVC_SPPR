986dbcfc57b24985e1d80492e395153e8c8db22d
ksu_kotovaa <ksu_kotovaa@mail.ru>
2024-09-30 10:09:12 +0300

    Add menu and paging

--- WEB_253503_KOTOVA/Controllers/ProductController.cs
+++ WEB_253503_KOTOVA/Controllers/ProductController.cs
@@ -2,8 +2,6 @@
 using WEB_253503_KOTOVA.Domain.Entities;
 using WEB_253503_KOTOVA.UI.Services.CategoryService;
 using WEB_253503_KOTOVA.UI.Services.ProductService;
-using System.Threading.Tasks;
-using WEB_253503_KOTOVA.Domain.Models;
 
 namespace WEB_253503_KOTOVA.Controllers
 {
@@ -18,36 +16,28 @@ namespace WEB_253503_KOTOVA.Controllers
             _categoryService = categoryService;
         }
 
-        public async Task<IActionResult> Index(string? category)
+        public async Task<IActionResult> Index(string? category, int pageNo = 1)
         {
-            // Получаем список категорий 
+            // Получаем список категорий
             var categoriesResponse = await _categoryService.GetCategoryListAsync();
             if (!categoriesResponse.Successfull)
                 return NotFound(categoriesResponse.ErrorMessage);
 
-            var categories = categoriesResponse.Data;
-
-            // Получаем продукты, отфильтрованные по категории 
-            var productResponse = await _productService.GetProductListAsync(category);
+            // Получаем продукты
+            var productResponse = await _productService.GetProductListAsync(category, pageNo);
             if (!productResponse.Successfull)
                 return NotFound(productResponse.ErrorMessage);
 
-            var products = productResponse.Data.Items;
+            // Ищем текущую категорию по её нормализованному имени
+            // var currentCategory = categoriesResponse.Data.FirstOrDefault(c => c.NormalizedName == category);
 
-            // Создаем ListModel с полученными данными 
-            var listModel = new ListModel<Dish>
-            {
-                Items = products,
-                CurrentPage = 1, // Установите номер текущей страницы, если требуется 
-                TotalPages = 1   // Поставьте количество страниц, если требуется 
-            };
+            // В ViewData["currentCategory"] передаем нормальное имя категории или "Все"
+            //ViewData["currentCategory"] = currentCategory != null ? currentCategory.Name : "Все";
+            ViewData["currentCategory"] = category != null ? category: "Все";
+            // Сохраняем список категорий в ViewBag для отображения в представлении
+            ViewBag.Categories = categoriesResponse.Data;
 
-            // Передаем текущую категорию и список категорий в представление 
-            var currentCategory = categories.FirstOrDefault(c => c.NormalizedName == category);
-            ViewData["currentCategory"] = currentCategory != null ? currentCategory.Name : "Все"; // Если null, то устанавливаем "Все" 
-            ViewBag.Categories = categories;
-
-            return View(listModel); // Возвращаем модель правильного типа 
+            return View(productResponse.Data);
         }
 
     }
